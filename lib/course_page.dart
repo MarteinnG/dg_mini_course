8c377bbd6baf88886ca8d7eecbc2238ae74f21b2
MarteinnG <101052414+MarteinnG@users.noreply.github.com>
2022-05-26 21:01:30 +0000

    fifteen commit
    Course Page created

---
+++ lib/course_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'constants.dart';
+import 'general_chapter1.dart';
+import 'main.dart';
+
+class CoursePage extends StatefulWidget {
+  @override
+  _CoursePageState createState() => _CoursePageState();
+}
+
+class _CoursePageState extends State<CoursePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                welcomeText,
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontStyle: FontStyle.italic,
+                  fontSize: 18.5,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 35.0,
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: GestureDetector(
+              child: FlatButton(
+                textColor: Colors.white,
+                color: Colors.blue,
+                child: Text(
+                  'Start the course',
+                  style: frontpageTextStyle,
+                ),
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => General()));
+                },
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:dg_mini_course/constants.dart';
 import 'package:dg_mini_course/course_brain.dart';
 import 'package:flutter/material.dart';
-import 'general_chapter1.dart';
 import 'constants.dart';
+import 'course_page.dart';
 
 CourseBrain courseBrain = CourseBrain();
 
@@ -33,56 +33,6 @@ class MiniCourse extends StatelessWidget {
   }
 }
 
-class CoursePage extends StatefulWidget {
-  @override
-  _CoursePageState createState() => _CoursePageState();
-}
 
-class _CoursePageState extends State<CoursePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Expanded(
-          flex: 5,
-          child: Padding(
-            padding: EdgeInsets.all(10.0),
-            child: Center(
-              child: Text(
-                welcomeText,
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                  fontStyle: FontStyle.italic,
-                  fontSize: 18.5,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-          ),
-        ),
-        SizedBox(
-          height: 35.0,
-        ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: GestureDetector(
-              child: FlatButton(
-                textColor: Colors.white,
-                color: Colors.blue,
-                child: Text(
-                  'Start the course',
-                  style: frontpageTextStyle,
-                ),
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => General()));
-                },
-              ),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-}
+
+
